a48dd7dada182265af0038c7d6f39222e0b9216d
rora0320 <83759908+rora0320@users.noreply.github.com>
2022-07-28 18:23:07 +0900

    카메라 접근/ 웹뷰 처음시도
    카메라 접근/ 웹뷰 처음시도

---
+++ components/webview.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {WebView} from 'react-native-webview';
+
+const WebViewTest = () => {
+  return <WebView source={{uri: 'https://developjkong.github.io/react-mobile'}} />;
+};
+
+export default WebViewTest;
